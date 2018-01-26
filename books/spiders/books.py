ef841bdd84e5225a3270aa88850f6bdd1b505e11
hong853 <chakhong.lam@gmail.com>
2018-01-26 23:07:35 +0800

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -4,27 +4,31 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["www.motanku.com"]
     start_urls = [
-        'http://books.toscrape.com/',
+        'https://www.motanku.com/',
     ]
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+        # for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
+		for book_url in response.css("div.sidebar > div.novelslist1 > div.content > ul > li > a ::attr(href)").extract(): #image_url
             yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        # next_page = response.css("li.next > a ::attr(href)").extract_first()
+        # if next_page:
+            # yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
     def parse_book_page(self, response):
         item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
+        book = response.css("div.box_con")
+        item["title"] = book.css("#info > h1 ::text").extract_first()
+		item["author"] = book.css("p ::text").extract_first()
+		item["description"] = books.css("#intro ::text").extract_first()
+		
+        # item['category'] = response.xpath(
+            # "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
+        # ).extract_first()
+        # item['description'] = response.xpath(
+            # "//div[@id='product_description']/following-sibling::p/text()"
+        # ).extract_first()
+        # item['price'] = response.css('p.price_color ::text').extract_first()
         yield item
